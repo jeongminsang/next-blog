fc642a5eb1909cf7c2dfcd69ac657246c7cb54ec
jeongminsang <zjzj@hanmail.net>
2023-07-24 23:22:52 +0900

    feat: styled-components 추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,26 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { getAllPostData } from './libs/api';
 import Link from "next/link"
+import { MainContainer, Title, SubTitle, PostTitle, PostList, PostBody } from "./styles/pages/Home"
 
 export default function Home() {
   const posts = getAllPostData();
   return (
-    <div>
-      <ul>
+    <MainContainer>
+      <Title>Minsang's Blog</Title>
+      <SubTitle>공부하고 경험한 내용을 이곳에 기록합니다.</SubTitle>
+      <PostTitle>All Posts({posts.length})</PostTitle>
+      <PostList>
         {posts.map((post: any, i: number) => (
-          <li key={i}>
+          <PostBody key={i}>
             <Link as={`/${post.postId}`} href={`/${post.postId}`}>
               <div>{post.title}</div>
-              <div>{post.date}</div>
               <div>{post.description}</div>
+              <div>{post.date}</div>
             </Link>
-          </li>
+          </PostBody>
         ))}
-      </ul>
-    </div>
+      </PostList>
+    </MainContainer>
   )
 }
