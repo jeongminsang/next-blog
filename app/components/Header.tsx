2aec49b05fd9f0cf97957e796ad01a5a9edd3405
jeongminsang <zjzj@hanmail.net>
2023-07-25 04:41:57 +0900

    fix: server component 변경

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { MainContainer, HeaderIcons, CustomImage } from '../styles/components/Header';
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
