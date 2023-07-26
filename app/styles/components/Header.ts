dd9d645e45253692ffc56d2582e0bc7675007ed9
jeongminsang <zjzj@hanmail.net>
2023-07-26 23:44:55 +0900

    refactor: 헤더 아이콘 버튼 추가

--- app/styles/components/Header.ts
+++ app/styles/components/Header.ts
@@ -12,14 +12,41 @@ export const MainContainer = styled.div`
   height: 70px;
 `;
 
-export const HeaderIcons = styled.div`
+export const HeaderTitle = styled.h4`
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: var(--hv-cr);
+    transition: background-color 0.2s ease;
+  }
+`;
+
+export const BlackThemeIcons = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   padding-left: 10px;
   margin: 10px;
+  display: var(--dis--light);
+`;
+
+export const LightThemeIcons = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 10px;
+  margin: 10px;
+  display: var(--dis--black);
 `;
 
 export const CustomImage = styled(Image)`
   margin: 10px;
+  padding: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: var(--hv-cr);
+    transition: background-color 0.2s ease;
+  }
 `;
