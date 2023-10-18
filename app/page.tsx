b1aa56193d9ce33e4ebe6681c32d3c3798b60aec
jeongminsang <zjzj@hanmail.net>
2023-10-19 01:41:51 +0900

    feat: 메인페이지 추가, 스타일 파일명 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
-import { MainContainer, Title, SubTitle } from "./styles/pages/Home"
+import { MainContainer, Title, ResumeImg, SubTitle, Contents } from "./styles/pages/Home"
 
 export default function Home() {
   return (
     <MainContainer>
+      <ResumeImg
+        width={1000}
+        height={1000}
+        alt="프로필 사진"
+        src="/me.jpg"
+      />
       <Title>Minsang Jeong</Title>
       <SubTitle>프론트엔드 개발자 정민상입니다.</SubTitle>
-
+      <Contents>
+        저는 TypeScript, React, Next.js 에 관심이 많습니다.
+        <br />
+        문제를 마주하면 항상 깊은 고뇌와 근본적인 원인을 찾으려고 노력합니다.
+        <br />
+        요즘 독서와 운동을 즐겨하고 있습니다.
+        <br />
+        <br />
+        Contact 은 오른쪽 하단 버튼을 이용해 주세요.
+      </Contents>
     </MainContainer>
   )
 }
---
+++ app/styles/pages/Blog.ts
@@ -0,0 +1,69 @@
+"use client";
+
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 800px;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export const Title = styled.h1`
+  margin-top: 26px;
+  margin-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+export const SubTitle = styled.h4`
+  margin-top: 0px;
+  margin-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  font-weight: 100;
+`;
+
+export const PostTitle = styled.h3`
+  margin-top: 10px;
+  margin-bottom: 0px;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+export const PostList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  height: 580px;
+`;
+
+export const PostBody = styled.li`
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 10px;
+  transition: transform 0.2s ease;
+  &:hover {
+    transform: translateY(-5px);
+    background-color: var(--hv-cr);
+    transition: transform 0.2s ease;
+  }
+  @media screen and (max-width: 800px) {
+    margin-top: 0px;
+    margin-bottom: 0px;
+    margin: 10px;
+  }
+`;
+
+export const PostInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  div {
+    display: flex;
+    align-items: center;
+    margin-right: 5px;
+  }
+`;
