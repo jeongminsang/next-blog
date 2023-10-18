39e65371b338e44888b63540cda127e0c30fab88
jeongminsang <zjzj@hanmail.net>
2023-10-18 23:48:52 +0900

    feat: project 카드 추가

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,21 +1,36 @@
 "use client"
 
 import Image from "next/image";
+import { CardContainer, CardImg, CardContents, CardTitle, ProjectStack, ButtonContainer, MoreButton } from "../styles/pages/Projects"
+import router from "next/router";
 
 const ProjectCard = (projectdata: any) => {
-  // console.log(projectdata.projectdata);
-  console.log(projectdata.projectdata.cover.external);
+  // console.log(projectdata.projectdata.properties.GitHub.rich_text[0].text.content);
+
+  const goGitHubHandle = () => {
+    window.open(`${projectdata.projectdata.properties.GitHub.rich_text[0].text.content}`)
+  }
+
   return (
-    <>
-      <div>{projectdata.projectdata.properties.Name.title[0].plain_text}</div>
-      <Image
+    <CardContainer>
+      {/* <div>{projectdata.projectdata.properties.Name.title[0].plain_text}</div> */}
+      <CardImg
         src={projectdata.projectdata.cover.external.url}
         alt="cover image"
         width={1000}
         height={1000}
-      // draggable={false}
+        draggable={false}
       />
-    </>
+      <CardContents>
+        <CardTitle>
+          {projectdata.projectdata.properties.Name.title[0].plain_text}
+        </CardTitle>
+        <ProjectStack>nextjs</ProjectStack>
+        <ButtonContainer>
+          <MoreButton onClick={goGitHubHandle}>LEARN MORE</MoreButton>
+        </ButtonContainer>
+      </CardContents>
+    </CardContainer>
   )
 }
 export default ProjectCard;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,6 @@
 import ProjectCard from "../components/ProjectCard";
 import { getProjectData } from "../libs/api";
-import { MainContainer, Title } from "../styles/pages/Home"
+import { MainContainer, Title, ProjectContainer } from "../styles/pages/Projects"
 
 export default async function Projects() {
   const data = await getProjectData();
@@ -8,10 +8,11 @@ export default async function Projects() {
   return (
     <MainContainer>
       <Title>Projects</Title>
-      {data && data.props.data.results.map((projectdata: any, index: number) => (
-        <ProjectCard key={index} projectdata={projectdata} />
-      ))}
-
+      <ProjectContainer>
+        {data && data.props.data.results.map((projectdata: any, index: number) => (
+          <ProjectCard key={index} projectdata={projectdata} />
+        ))}
+      </ProjectContainer>
     </MainContainer>
   );
 }
