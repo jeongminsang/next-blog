f203e3d6f666fa78797671ae998d664974098682
jeongminsang <zjzj@hanmail.net>
2023-11-10 23:54:32 +0900

    docs: 프로젝트 이미지 파일 및 배열 추가

---
+++ app/libs/ProjectImgs.ts
@@ -0,0 +1,27 @@
+export const projectImgs = [
+  {
+    id: 1,
+    projectName: "Minsang.dev",
+    coverimage: "/projectimg/blogcover.png",
+  },
+  {
+    id: 2,
+    projectName: "PlaNet",
+    coverimage: "/projectimg/planetcover.png",
+  },
+  {
+    id: 3,
+    projectName: "Wanted-Internship-TodoList",
+    coverimage: "/projectimg/todolist1.png",
+  },
+  {
+    id: 4,
+    projectName: "Green Circle",
+    coverimage: "/projectimg/greencircle1.png",
+  },
+  {
+    id: 5,
+    projectName: "7391-MBTI",
+    coverimage: "/projectimg/mbticover.png",
+  },
+];
