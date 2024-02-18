6fa9e9cec6ecfc42a22770e600d370a650458c36
Minsang Jung <min@foodly.tech>
2024-02-18 21:44:38 +0900

    add header stylex

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { MainContainer, HeaderTitle, SideButtons, RouterButtons, RouterButton, CustomImage, BlackThemeIcons, LightThemeIcons } from '../styles/components/Header';
 import { useRouter, usePathname } from 'next/navigation';
+import stylex from '@stylexjs/stylex';
+import Image from "next/image";
 
 const Header = () => {
   const router = useRouter()
@@ -12,7 +13,6 @@ const Header = () => {
     document.body.dataset.theme = newTheme;
   };
   const params = usePathname();
-
 
   const goHomeHandle = () => {
     router.push('/');
@@ -29,45 +29,128 @@ const Header = () => {
   }
 
   return (
-    <MainContainer>
-      <HeaderTitle onClick={goHomeHandle}>minsang.dev</HeaderTitle>
-      <SideButtons>
-        <RouterButtons>
-          <RouterButton onClick={goBlogHandle} params={params === '/blog' ? 'true' : 'false'}>blog</RouterButton>
-          <RouterButton onClick={goProjectHandle} params={params === '/projects' ? 'true' : 'false'}>projects</RouterButton>
-        </RouterButtons>
-        <BlackThemeIcons>
-          <CustomImage onClick={themeModeHandle}
+    <div {...stylex.props(style.MainContainer)}>
+      <h4 {...stylex.props(style.HeaderTitle)} onClick={goHomeHandle}>minsang.dev</h4>
+      <div {...stylex.props(style.SideButtons)}>
+        <div {...stylex.props(style.RouterButtons)}>
+          <div {...stylex.props(style.RouterButton(params === '/blog' ? 'true' : 'false'))} onClick={goBlogHandle}>blog</div>
+          <div {...stylex.props(style.RouterButton(params === '/projects' ? 'true' : 'false'))} onClick={goProjectHandle}>projects</div>
+        </div>
+        <div {...stylex.props(style.BlackThemeIcons)}>
+          <Image {...stylex.props(style.CustomImage)} onClick={themeModeHandle}
             width={40}
             height={40}
             alt="밝은 모드로 변경"
             src="/sunny.svg"
           />
-          <CustomImage onClick={goGitHubHandle}
+          <Image {...stylex.props(style.CustomImage)} onClick={goGitHubHandle}
             width={40}
             height={40}
             alt="GitHub-White"
             src="/github-mark-white.svg"
           />
-        </BlackThemeIcons>
-        <LightThemeIcons>
-          <CustomImage onClick={themeModeHandle}
+        </div>
+        <div {...stylex.props(style.LightThemeIcons)}>
+          <Image {...stylex.props(style.CustomImage)} onClick={themeModeHandle}
             width={40}
             height={40}
             alt="어두운 모드로 변경"
             src="/dark.svg"
           />
-          <CustomImage onClick={goGitHubHandle}
+          <Image {...stylex.props(style.CustomImage)} onClick={goGitHubHandle}
             width={40}
             height={40}
             alt="GitHub-Black"
             src="/github-mark.svg"
           />
-        </LightThemeIcons>
-      </SideButtons>
+        </div>
+      </div>
 
 
-    </MainContainer>
+    </div>
   )
 }
 export default Header;
+
+const MEDIA_MOBILE = '@media (max-width: 800px)' as const;
+
+const style = stylex.create({
+  MainContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: {
+      default: '800px',
+      [MEDIA_MOBILE]: '100%'
+    },
+    height: '70px',
+    margin: '0 auto',
+  },
+  HeaderTitle: {
+    padding: '10px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    backgroundColor: {
+      ':hover': 'var(--hv-cr);',
+    },
+    transitionProperty: 'background-color',
+    transitionDuration: '200ms',
+  },
+  SideButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    height: '70px',
+    margin: '0 auto',
+    width: {
+      default: '800px',
+      [MEDIA_MOBILE]: '100%'
+    },
+  },
+  RouterButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderRadious: '5px',
+  },
+  RouterButton: (params) => ({
+    fontSize: '17px',
+    padding: '8px',
+    margin: '5px',
+    backgroundColor: {
+      default: 'rgba(0, 0, 0, 0)',
+      ':hover': 'var(--hv-cr);',
+    },
+    color: params === "true" ? "var(--text--color)" : "grey",
+    borderRadious: '5px',
+    cursor: 'pointer',
+    transitionProperty: 'background-color',
+    transitionDuration: '200ms',
+  }),
+  BlackThemeIcons: {
+    display: 'var(--dis--light)',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: '10px',
+    margin: '10px 0px 10px 0px',
+  },
+  LightThemeIcons: {
+    display: 'var(--dis--black)',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: '10px',
+    margin: '10px 0px 10px 0px',
+  },
+  CustomImage: {
+    margin: '10px',
+    padding: '5px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    backgroundColor: {
+      ':hover': 'var(--hv-cr)',
+    },
+    transitionProperty: 'background-color',
+    transitionDuration: '200ms',
+  },
+});
